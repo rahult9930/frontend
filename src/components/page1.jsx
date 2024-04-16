c60aacf50929597fa8658a13a3088b5ef2f97475
Rahul Tailor <rahultailorjaipur000@gmail.com>
2024-04-16 22:23:36 +0530

    responsive

--- src/components/page1.jsx
+++ src/components/page1.jsx
@@ -1,112 +1,106 @@
 import { Button, Typography, Grid, Box } from '@mui/material';
 import React from 'react';
-import preview2 from './img/preview2.jpg'; 
+import preview2 from './img/preview2.jpg';
+import { useMediaQuery } from 'react-responsive';
 
 const Page1 = (props) => {
+  const isTabletOrMobile = useMediaQuery({ query: '(max-width: 800px)' });
+
   return (
-    <div >
-       <Button variant="text" size="medium" style={{
-        marginLeft:"87%",
-        marginTop:"3%",
+    <div>
+      <Button variant="text" size="medium" style={{
+        marginLeft: isTabletOrMobile ?"80%":"87%",
+        marginTop: "3%",
         textTransform: 'none'
-       }} onClick={() => window.location="/"}>
+      }} onClick={() => window.location = "/"}>
         <Typography style={{
-          fontFamily:'Open Sans',
-          color:"#6574CF",
+          fontFamily: 'Open Sans',
+          color: "#6574CF",
           fontWeight: 'bold'
         }}>Skip</Typography>
-       </Button>
+      </Button>
 
-       <Box sx={{ flexGrow: 1 }}>
+      <Box sx={{ flexGrow: 1 }}>
         <Grid container spacing={2}>
-          <Grid item xs={6} >
-            <Grid container direction="column" style={{ display: 'flex' , marginLeft:170 ,marginTop:80 }}>
+          <Grid item xs={12} md={6}>
+            <Grid container direction="column" style={{ display: 'flex', marginLeft: isTabletOrMobile ? 48 : 170, marginTop: isTabletOrMobile ? 20 : 80 }}>
               <Grid item>
-                <Typography variant='h2' style={{
-                  fontFamily:'Open Sans',
-                  color:"#6574CF",
+                <Typography variant={isTabletOrMobile ? 'h3' : 'h2'} style={{
+                  fontFamily: 'Open Sans',
+                  color: "#6574CF",
                   fontWeight: 'bold'
                 }}>
                   Welcome
                 </Typography>
               </Grid>
               <Grid item>
-                <Typography variant='h3' style={{
-                  fontFamily:'Open Sans',
-                  color:"#222B45",
+                <Typography variant={isTabletOrMobile ? 'h4' : 'h3'} style={{
+                  fontFamily: 'Open Sans',
+                  color: "#222B45",
                   fontWeight: 'medium'
                 }}>
                   to Prescribal
                 </Typography>
               </Grid>
               <Grid item>
-                <Typography variant='h5' style={{
-                  fontFamily:'Open Sans',
-                  color:"#687482",
-                  marginTop:10,
-                  width:"35vw"
+                <Typography variant={isTabletOrMobile ? 'h6' : 'h5'} style={{
+                  fontFamily: 'Open Sans',
+                  color: "#687482",
+                  marginTop: 10,
+                  width: isTabletOrMobile ? "90vw" : "35vw",
+                  marginRight:isTabletOrMobile ? 10:0
                 }}>
                   Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.
                 </Typography>
               </Grid>
             </Grid>
           </Grid>
-          <Grid item xs={6} style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+          <Grid item xs={12} md={6} style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
             <img src={preview2} alt="Doctor" style={{
-              width:"60%",
-              maxHeight:"90%",
-              marginRight:70
-            }}/>
+              width: isTabletOrMobile ? "80%" : "60%",
+              maxHeight: "90%",
+              marginRight: isTabletOrMobile ? 0 : 70
+            }} />
           </Grid>
         </Grid>
       </Box>
       <Box sx={{ flexGrow: 1 }}>
         <Grid container spacing={2}>
-          <Grid item xs={6} >
-          <div style={{marginLeft:170,marginTop:43 }}>
-     {/* <span style={{ fontSize: '18px',marginRight:4 }}><ion-icon name="laptop-sharp" size="medium" style={{
-        color:"#6574CF",
-      }}></ion-icon></span>  */}
-     {/* <span style={{ width: '20px', height: '40px', borderRadius: '8px', overflow: 'hidden', display: 'inline-block', boxShadow: '0px 0px 0px 4px rgba(101, 116, 207, 0.2)', marginRight: '4px' }}>
-    
-     </span> */}
-     <span  style={{ fontSize: '12px', color: '#6574CF',width:"40%", marginRight:"4px"}} >
-     <ion-icon name="ellipse-sharp" style={{marginRight:"-1.5%"}}></ion-icon>
-     <ion-icon name="square-sharp"></ion-icon>
-     <ion-icon name="ellipse-sharp" style={{marginLeft:"-1.5%"}}></ion-icon>
-     </span>
-<span style={{ fontSize: '12px', marginRight: '4px' }}>
-  <ion-icon name="ellipse-sharp" style={{ color: '#6574CF' }}></ion-icon>
-</span>
-<span style={{ fontSize: '12px', marginRight: '4px' }}>
-  <ion-icon name="ellipse-sharp" style={{ color: '#6574CF' }}></ion-icon>
-</span>
-
-
-      </div>
+          <Grid item xs={6} md={6}>
+            <div style={{ marginLeft: isTabletOrMobile ? 20 : 170, marginTop: isTabletOrMobile ? 20 : 43 }}>
+              <span style={{ fontSize: '12px', color: '#6574CF', width: isTabletOrMobile ? "90%" : "40%", marginRight: "4px" }} >
+                <ion-icon name="ellipse-sharp" style={{ marginRight:isTabletOrMobile?"-3%": "-1.5%" }}></ion-icon>
+                <ion-icon name="square-sharp"></ion-icon>
+                <ion-icon name="ellipse-sharp" style={{ marginLeft:isTabletOrMobile?"-3%": "-1.5%" }}></ion-icon>
+              </span>
+              <span style={{ fontSize: '12px', marginRight: '4px' }}>
+                <ion-icon name="ellipse-sharp" style={{ color: '#6574CF' }}></ion-icon>
+              </span>
+              <span style={{ fontSize: '12px', marginRight: '4px' }}>
+                <ion-icon name="ellipse-sharp" style={{ color: '#6574CF' }}></ion-icon>
+              </span>
+            </div>
           </Grid>
-          <Grid item xs={6} style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-          <Button variant="contained" size="medium" style={{
-        marginLeft: "20%",
-        marginTop: "3%",
-        textTransform: 'none',
-        backgroundColor:"#6574CF"
-      }} onClick={() => props.setPage(2)}>
-        <Typography style={{
-          fontFamily: 'Open Sans',
-          display: 'flex',
-          alignItems: 'center' // Align items vertically
-        }}>
-          <span>Next</span> {/* Include span inside Typography */}
-          <ion-icon name="arrow-forward-circle-outline" size="small" style={{marginLeft:4}}></ion-icon>
-        </Typography>
-      </Button>
+          <Grid item xs={6} md={6} style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+            <Button variant="contained" size="medium" style={{
+              marginLeft: isTabletOrMobile ? "20%" : "20%",
+              marginTop: "3%",
+              textTransform: 'none',
+              backgroundColor: "#6574CF"
+            }} onClick={() => props.setPage(2)}>
+              <Typography style={{
+                fontFamily: 'Open Sans',
+                display: 'flex',
+                alignItems: 'center' // Align items vertically
+              }}>
+                <span>Next</span> {/* Include span inside Typography */}
+                <ion-icon name="arrow-forward-circle-outline" size="small" style={{ marginLeft: 4 }}></ion-icon>
+              </Typography>
+            </Button>
           </Grid>
         </Grid>
       </Box>
-     
     </div>
-
   )
 }
 
