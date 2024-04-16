7907aca3d18ecedd18429eb376653ca793c89602
Rahul Tailor <rahultailorjaipur000@gmail.com>
2024-04-16 22:40:01 +0530

    ihuhi

--- src/components/page2.jsx
+++ src/components/page2.jsx
@@ -72,9 +72,9 @@ const Page2 = (props) => {
                 <ion-icon name="ellipse-sharp" style={{ color: '#6574CF' }}></ion-icon>
               </span>
               <span style={{ fontSize: '12px', color: '#6574CF', width: isTabletOrMobile ? "90%" : "40%", marginRight: "4px" }} >
-                <ion-icon name="ellipse-sharp" style={{ marginRight:isTabletOrMobile?"-15%": "-1.5%" }}></ion-icon>
+                <ion-icon name="ellipse-sharp" style={{ marginRight:isTabletOrMobile?"-25%": "-1.5%" }}></ion-icon>
                 <ion-icon name="square-sharp"></ion-icon>
-                <ion-icon name="ellipse-sharp" style={{ marginLeft:isTabletOrMobile?"-15%": "-1.5%" }}></ion-icon>
+                <ion-icon name="ellipse-sharp" style={{ marginLeft:isTabletOrMobile?"-25%": "-1.5%" }}></ion-icon>
               </span>
               <span style={{ fontSize: '12px', marginRight: '4px' }}>
                 <ion-icon name="ellipse-sharp" style={{ color: '#6574CF' }}></ion-icon>
